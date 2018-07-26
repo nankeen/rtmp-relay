e1007eb35d2162a920bd7f8515c9760fc46346e9
CK <nankeen@users.noreply.github.com>
2018-07-26 13:36:15 +0800

    updated to have hls stream now

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+/*
 const { NodeMediaCluster } = require('node-media-server')
 const numCPUs = require('os').cpus().length
 const config = {
@@ -19,3 +20,33 @@ const config = {
 
 var nmcs = new NodeMediaCluster(config)
 nmcs.run()
+*/
+const { NodeMediaServer } = require('node-media-server');
+
+const config = {
+  rtmp: {
+    port: 1935,
+    chunk_size: 60000,
+    gop_cache: true,
+    ping: 60,
+    ping_timeout: 30
+  },
+  http: {
+    port: 8000,
+    allow_origin: '*'
+  },
+  trans: {
+    ffmpeg: '/usr/local/bin/ffmpeg',
+    tasks: [
+      {
+        app: 'live',
+        ac: 'aac',
+        hls: true,
+        hlsFlags: '[hls_time=2:hls_list_size=3:hls_flags=delete_segments]',
+      }
+    ]
+  }
+};
+
+var nms = new NodeMediaServer(config)
+nms.run();
